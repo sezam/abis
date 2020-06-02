64a61df885d2a0e6dbd614d06fa6968b26a27443
zamorin_s_v <zamorin_s_v@goznak.ru>
2020-06-02 17:43:07 +0500

    1

--- src/restutils.cpp
+++ src/restutils.cpp
@@ -45,7 +45,7 @@ void print_json(json::value const& jvalue)
 	ss << "../log/" << std::put_time(std::localtime(&in_time_t), "%d-%m-%Y %H-%M-%S") << ".json.txt";
 
 	ofstream ofs(ss.str(), ios::out);
-	if(!ofs.is_open()) BOOST_LOG_TRIVIAL(debug) << "JSON saver error: " << std::strerror(errno);
+	if(!ofs.is_open()) BOOST_LOG_TRIVIAL(debug) << "JSON saver error: " << errno;
 	ofs << conversions::to_utf8string(jvalue.serialize());
 
 	ofs.close();
