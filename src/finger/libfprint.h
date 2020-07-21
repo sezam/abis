645cb5122cc03032617ab3b19b421d3d5c5a2a73
Заморин Сергей Витальевич <zamorin_s_v@goznak.ru>
2020-07-21 09:00:54 +0500

    change finger gost compare function

--- src/finger/libfprint.h
+++ src/finger/libfprint.h
@@ -11,4 +11,7 @@ extern "C" int matchSegments(struct fp_img* image1, struct fp_img* image2);
 extern "C" int matchSegmentsImgTemplate(struct fp_img* image1, unsigned char* tmpl);
 extern "C" int matchSegmentsTemplate(unsigned char* tmpl1, int cx1, int cy1,  unsigned char* tmpl2,int cx2,int cy2);
 
+extern "C" int matchSegmentsTemplateArea(unsigned char* tmpl1, int cx1, int cy1,  unsigned char* tmpl2,int cx2,int cy2);
+extern "C" int matchSegmentsTemplateCilindre(unsigned char* tmpl1, int cx1, int cy1,  unsigned char* tmpl2,int cx2,int cy2);
+
 #endif // __MAIN_H__
