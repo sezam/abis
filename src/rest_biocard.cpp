da95404b4fd146b348bc905c385219843b408560
zamorin_s_v <zamorin_s_v@goznak.ru>
2020-09-22 09:35:10 +0500

    обращения для внесения изменений в таблицы участвующие в поиске должны быть в соединении к БД без транзакций

--- src/rest_biocard.cpp
+++ src/rest_biocard.cpp
@@ -96,11 +96,14 @@ void biocard_put(http_request request)
 		request,
 		[&](json::value const& req_json, json::value& answer)
 		{
-			PGconn* db = nullptr;
+			PGconn* transact_db = nullptr;
+			PGconn* readonly_db = nullptr;
 			try
 			{
-				db = db_open();
-				db_tx_begin(db);
+				readonly_db = db_open();
+
+				transact_db = db_open();
+				db_tx_begin(transact_db);			
 
 				uuid gid;
 				int bc_id = 0;
@@ -112,7 +115,7 @@ void biocard_put(http_request request)
 					boost::uuids::random_generator gen;
 					gid = gen();
 
-					bc_id = db_add_bc(db, to_string(gid).c_str(), "");
+					bc_id = db_add_bc(transact_db, to_string(gid).c_str(), "");
 					if (bc_id <= 0) res = -1;
 				}
 				if (sp.size() > 0)
@@ -120,7 +123,7 @@ void biocard_put(http_request request)
 					string_generator gen;
 					gid = gen(utility::conversions::to_utf8string(sp[0]));
 
-					bc_id = db_get_bc_by_gid(db, to_string(gid).c_str());
+					bc_id = db_get_bc_by_gid(transact_db, to_string(gid).c_str());
 					if (bc_id <= 0) res = -2;
 				}
 
@@ -152,10 +155,10 @@ void biocard_put(http_request request)
 
 					if (step && tmp_type == ABIS_FACE_TEMPLATE)
 					{
-						db_sp_begin(db, "face_template");
+						db_sp_begin(transact_db, "face_template");
 						if (step)
 						{
-							step = db_search_face_tmp(db, tmp_in, tmp_id) > 0;
+							step = db_search_face_tmp(readonly_db, tmp_in, tmp_id) > 0;
 							if (!step) res = -101;
 							if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error search face template - " << res;
 						}
@@ -170,7 +173,7 @@ void biocard_put(http_request request)
 							if (step)
 							{
 								memset(tmp_db, 0, ABIS_TEMPLATE_SIZE);
-								step = db_face_tmp_by_id(db, tmp_id, tmp_db) > 0;
+								step = db_face_tmp_by_id(readonly_db, tmp_id, tmp_db) > 0;
 								if (!step) res = -103;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error get face template - " << res;
 							}
@@ -181,15 +184,15 @@ void biocard_put(http_request request)
 								if (step && fscore >= ABIS_FACE_THRESHOLD)
 								{
 									char bc_gid[50];
-									step = db_get_bc_for_tmp(db, tmp_type, tmp_id, bc_gid) == 0;
+									step = db_get_bc_for_tmp(transact_db, tmp_type, tmp_id, bc_gid) == 0;
 									if (!step) res = -104;
 									if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: face template already in biocard - " << res;
 								}
 								if (step && fscore <= ABIS_EQUAL_THRESHOLD)
 								{
-									tmp_id = db_get_face_seq(db);
+									tmp_id = db_get_face_seq(readonly_db);
 									step = tmp_id > 0;
-									if (step) step = db_insert_face_tmp(db, tmp_in, tmp_id);
+									if (step) step = db_insert_face_tmp(readonly_db, tmp_in, tmp_id);
 									if (!step) res = -105;
 									if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error insert face template - " << res;
 								}
@@ -199,22 +202,22 @@ void biocard_put(http_request request)
 
 						if (step)
 						{
-							step = db_add_link(db, ABIS_FACE_TEMPLATE, tmp_id, bc_id) > 0;
+							step = db_add_link(transact_db, ABIS_FACE_TEMPLATE, tmp_id, bc_id) > 0;
 							if (!step) res = -106;
 							if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error add face template to biocard - " << res;
 						}
 
 						if (step) inserted++;
-						if (!step) db_sp_rollback(db, "face_template");
+						if (!step) db_sp_rollback(transact_db, "face_template");
 					}
 
 
 					if (step && tmp_type == ABIS_FINGER_GOST_TEMPLATE)
 					{
-						db_sp_begin(db, "finger_template");
+						db_sp_begin(transact_db, "finger_template");
 						if (step)
 						{
-							step = db_search_finger_tmp(db, tmp_in, tmp_id) > 0;
+							step = db_search_finger_tmp(readonly_db, tmp_in, tmp_id) > 0;
 							if (!step) res = -201;
 							if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error search finger template - " << res;
 						}
@@ -231,7 +234,7 @@ void biocard_put(http_request request)
 							if (step)
 							{
 								memset(tmp_db, 0, ABIS_TEMPLATE_SIZE);
-								step = db_finger_tmp_by_id(db, tmp_id, tmp_db) > 0;
+								step = db_finger_tmp_by_id(readonly_db, tmp_id, tmp_db) > 0;
 								if (!step) res = -203;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error get finger template - " << res;
 							}
@@ -245,15 +248,15 @@ void biocard_put(http_request request)
 						{
 							if (score_tmp < ABIS_EQUAL_THRESHOLD)
 							{
-								if (step) step = (tmp_id = db_get_finger_seq(db)) > 0;
+								if (step) step = (tmp_id = db_get_finger_seq(readonly_db)) > 0;
 								if (!step) res = -204;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error get finger sequence - " << res;
 
-								if (step) step = db_insert_finger_tmp(db, tmp_in, tmp_id) > 0;
+								if (step) step = db_insert_finger_tmp(readonly_db, tmp_in, tmp_id) > 0;
 								if (!step) res = -205;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error insert finger template - " << res;
 
-								if (step) step = db_append_finger_gost(db, (uchar*)tmp_in + ABIS_TEMPLATE_SIZE, tmp_id) > 0;
+								if (step) step = db_append_finger_gost(readonly_db, ((uchar*)tmp_in) + ABIS_TEMPLATE_SIZE, tmp_id) > 0;
 								if (!step) res = -206;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error insert finger template 2 - " << res;
 							}
@@ -267,7 +270,7 @@ void biocard_put(http_request request)
 								if (step)
 								{
 									memset(db_gost, 0, ABIS_FINGER_TMP_GOST_SIZE);
-									int gost_len = db_gost_tmp_by_id(db, tmp_id, db_gost);
+									int gost_len = db_gost_tmp_by_id(readonly_db, tmp_id, db_gost);
 									is_gost = gost_len == ABIS_FINGER_TMP_GOST_SIZE;
 									step = gost_len >= 0;
 									if (!step) res = -207;
@@ -286,7 +289,7 @@ void biocard_put(http_request request)
 								if (score_gost >= ABIS_FINGER_GOST_THRESHOLD)
 								{
 									char bc_gid[50];
-									step = db_get_bc_for_tmp(db, tmp_type, tmp_id, bc_gid) == 0;
+									step = db_get_bc_for_tmp(transact_db, tmp_type, tmp_id, bc_gid) == 0;
 									if (!step) res = -208;
 									if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: finger template already in biocard - " << res;
 								}
@@ -299,7 +302,7 @@ void biocard_put(http_request request)
 							}
 							else
 							{
-								if (step) step = db_append_finger_gost(db, ((uchar*)tmp_in) + ABIS_TEMPLATE_SIZE, tmp_id) > 0;
+								if (step) step = db_append_finger_gost(readonly_db, ((uchar*)tmp_in) + ABIS_TEMPLATE_SIZE, tmp_id) > 0;
 								if (!step) res = -210;
 								if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error insert finger template 2 - " << res;
 							}
@@ -307,12 +310,12 @@ void biocard_put(http_request request)
 
 						if (step)
 						{
-							step = db_add_link(db, ABIS_FINGER_GOST_TEMPLATE, tmp_id, bc_id);
+							step = db_add_link(transact_db, ABIS_FINGER_GOST_TEMPLATE, tmp_id, bc_id);
 							if (step) inserted++;
 							if (!step) res = -211;
 							if (!step) BOOST_LOG_TRIVIAL(debug) << "biocard_put: error insert finger template in biocard - " << res;
 						}
-						else db_sp_rollback(db, "finger_template");
+						else db_sp_rollback(transact_db, "finger_template");
 					}
 
 					res &= 0x4000;
@@ -324,14 +327,15 @@ void biocard_put(http_request request)
 					json_out[i] = json_row;
 
 					if (tmp_in != nullptr) free(tmp_in);
+
 				}
 				//if (inserted > 0)
 				if (step)
 				{
-					db_tx_commit(db);
+					db_tx_commit(transact_db);
 					answer[ELEMENT_UUID] = json::value::string(conversions::to_string_t(to_string(gid)));
 				}
-				else db_tx_rollback(db);
+				else db_tx_rollback(transact_db);
 
 				answer[ELEMENT_VALUE] = json_out;
 				answer[ELEMENT_RESULT] = json::value::boolean(step);
@@ -345,7 +349,8 @@ void biocard_put(http_request request)
 			{
 				JSON_EXCEPTION(answer, ec.what());
 			}
-			db_close(db);
+			db_close(transact_db);
+			db_close(readonly_db);
 		});
 
 	request.reply(sc, "");
